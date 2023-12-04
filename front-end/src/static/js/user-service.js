dfca8bc6d236d45521e8bcbff9224deed754d258
Tước Thiện Đào <tuoc.dao@adnovum.vn>
2023-12-04 17:59:00 +0700

    Update user-service with nevistool code for return sectoken information

--- front-end/src/static/js/user-service.js
+++ front-end/src/static/js/user-service.js
@@ -1,1 +1,17 @@
+function getViaRest(path, contentType) {
+    var http = new XMLHttpRequest();
+    http.open('GET', root + path, true);
 
+    http.onreadystatechange = function () {
+        if (http.readyState == 4) {
+            var resSpace = document.getElementById('result');
+            resSpace.innerHTML = http.responseText;
+        };
+    };
+
+    http.setRequestHeader('Accept', contentType);
+    http.send();
+    return false;
+}
+
+<input type="button" onclick="getViaRest('rest/user', 'application/json');" value="user info" />	
